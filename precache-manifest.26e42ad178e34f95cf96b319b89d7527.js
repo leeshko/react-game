e494d016a3f3d529a60c603732ae50b507f554e6
leeshko <pumpkin@tut.by>
2021-03-06 09:48:55 +0300

    Updates

---
+++ precache-manifest.26e42ad178e34f95cf96b319b89d7527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b50c5cbbaebf78a81d8",
+    "url": "/react-game/static/css/main.fcdfc2e3.chunk.css"
+  },
+  {
+    "revision": "6b50c5cbbaebf78a81d8",
+    "url": "/react-game/static/js/main.6b50c5cb.chunk.js"
+  },
+  {
+    "revision": "7dd7f5c31f8c525737d5",
+    "url": "/react-game/static/js/1.7dd7f5c3.chunk.js"
+  },
+  {
+    "revision": "cda9c7c5162c773fb401",
+    "url": "/react-game/static/js/runtime~main.cda9c7c5.js"
+  },
+  {
+    "revision": "30e462bea6c98e1835e9c0b07aac26f7",
+    "url": "/react-game/static/media/logo.30e462be.png"
+  },
+  {
+    "revision": "205904ee42453f2a11ea5a28b0a68121",
+    "url": "/react-game/index.html"
+  }
+];
